68e576603c57864e1887e7e01e44b13dd9750774
senseiigarou-wq <senseiigarou@gmail.com>
2025-11-30 14:10:32 +0800

    Delete src directory

--- src/src/data/vouchers.ts
+++
@@ -1,5 +0,0 @@
-export const vouchers = [
-  { code: "ABC123", minutes: 120 }, // 2 hours
-  { code: "XYZ999", minutes: 300 }, // 5 hours
-  { code: "TEST10", minutes: 10 }   // 10 mins (testing)
-];
