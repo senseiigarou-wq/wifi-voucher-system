f8391a865cc9a2818baef83b591cc600cdfe4b41
senseiigarou-wq <senseiigarou@gmail.com>
2025-11-30 14:09:48 +0800

    Delete src/src/data/utils/components directory

--- src/src/data/utils/components/WifiStatus.tsx
+++
@@ -1,7 +0,0 @@
-export default function WifiStatus({ active }: { active: boolean }) {
-  return (
-    <div className="box">
-      <h2>{active ? "CONNECTED" : "NOT CONNECTED"}</h2>
-    </div>
-  );
-}
