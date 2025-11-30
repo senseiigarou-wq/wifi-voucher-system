44d6db0bdbfd3d47f9f76e7e14ac754b3ca7db0e
senseiigarou-wq <senseiigarou@gmail.com>
2025-11-30 14:07:03 +0800

    Add vouchers data with codes and durations

---
+++ src/src/data/vouchers.ts
@@ -0,0 +1,5 @@
+export const vouchers = [
+  { code: "ABC123", minutes: 120 }, // 2 hours
+  { code: "XYZ999", minutes: 300 }, // 5 hours
+  { code: "TEST10", minutes: 10 }   // 10 mins (testing)
+];
